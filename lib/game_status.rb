bcf0bebf307556558a15196471bb0c22f6f604b0
Joel Kelly <joel.kelly.philly@gmail.com>
2020-03-20 17:22:29 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -39,7 +39,8 @@ def over?(board)
 end
 
 def winner(board)
-  if won?(board)
-    board[won?(board)[0]]
-  end
+  #if won?(board)
+    #board[won?(board)[0]]
+  #end
+  won?(board) ? board[won?(board)[0]] : nil
 end
