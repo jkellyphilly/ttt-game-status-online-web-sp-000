c49f91ea03a3ce4409690eb589070eaeaf38a04c
Joel Kelly <joel.kelly.philly@gmail.com>
2020-03-20 16:45:54 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,6 +4,13 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
-top_row_win = [0, 1, 2]
-middle_row_win = [3, 4, 5]
-bottom_row_win = [6, 7, 8]
+WIN_COMBINATIONS = [
+  [0, 1, 2] # top row
+  [3, 4, 5] # middle row
+  [6, 7, 8] # bottom row
+  [0, 3, 6] # left column
+  [1, 4, 7] # middle column
+  [2, 5, 8] # right column
+  [6, 4, 2] # / diagonal
+  [0, 4, 8] # \ diagonal 
+]
