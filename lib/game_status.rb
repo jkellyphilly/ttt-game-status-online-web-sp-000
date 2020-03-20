0d3a64436c2ac6a6fa89b17343c36730baac7ccf
Joel Kelly <joel.kelly.philly@gmail.com>
2020-03-20 16:56:49 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -5,16 +5,18 @@ end
 
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [
-  [0, 1, 2] # top row
-  [3, 4, 5] # middle row
-  [6, 7, 8] # bottom row
-  [0, 3, 6] # left column
-  [1, 4, 7] # middle column
-  [2, 5, 8] # right column
-  [6, 4, 2] # / diagonal
+  [0, 1, 2], # top row
+  [3, 4, 5], # middle row
+  [6, 7, 8], # bottom row
+  [0, 3, 6], # left column
+  [1, 4, 7], # middle column
+  [2, 5, 8], # right column
+  [6, 4, 2], # / diagonal
   [0, 4, 8] # \ diagonal
 ]
 
 def won?(board)
-  
+  WIN_COMBINATIONS.each do | winning_combination |
+    if (board[winning_combination[0]] == "X" && board[winning_combination[1]] == "X" && board[winning_combination[2]] == "X") || (board[winning_combination[0]] == "X" && board[winning_combination[1]] == "X" && board[winning_combination[2]] == "X")
+  end
 end
