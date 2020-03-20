3b5e8f96228c397782b1997d063922a407dacb6f
Joel Kelly <joel.kelly.philly@gmail.com>
2020-03-20 17:28:10 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -16,15 +16,14 @@ WIN_COMBINATIONS = [
 ]
 
 def won?(board)
-  #WIN_COMBINATIONS.each do | winning_combination |
-    #if (board[winning_combination[0]] == "X" && board[winning_combination[1]] == "X" && board[winning_combination[2]] == "X") || (board[winning_combination[0]] == "O" && board[winning_combination[1]] == "O" && board[winning_combination[2]] == "O")
-      #return winning_combination
-    #else
-      #false
-    #end
-  #end
-  #false
-  board[winning_combination]
+  WIN_COMBINATIONS.each do | winning_combination |
+    if (board[winning_combination[0]] == "X" && board[winning_combination[1]] == "X" && board[winning_combination[2]] == "X") || (board[winning_combination[0]] == "O" && board[winning_combination[1]] == "O" && board[winning_combination[2]] == "O")
+      return winning_combination
+    else
+      false
+    end
+  end
+  false
 end
 
 def full?(board)
